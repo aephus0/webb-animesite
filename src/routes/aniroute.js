54d8eb549e24a637f40ef0d58c2d64ca4ec0036a
xukkz <wiggo.bergman@gmail.com>
2021-05-01 21:29:59 +0200

    ability to search by id

--- src/routes/aniroute.js
+++ src/routes/aniroute.js
@@ -14,7 +14,29 @@ router.get("/", async (req, res) => {
   console.log(docs);
 });
 
+router.get("/:id", async (req, res) => {
+  const specAnime = await Anime.findOne({
+    aniId: req.params.id,
+  });
+
+  if (specAnime === null) {
+    return res.json(
+      new FailRes({
+        aniId: "id not found",
+      })
+    );
+  }
+  res.send(specAnime);
+});
+
 router.post("/newanime", async (req, res) => {
+  if (req.body.title.trim() === "") {
+    return res.json(
+      new ErrorRes({
+        title: "title cannot be emtpy",
+      })
+    );
+  }
   const anime = await Anime.findOne({
     title: req.body.title,
   });
@@ -22,13 +44,17 @@ router.post("/newanime", async (req, res) => {
   if (anime !== null) {
     return res.json(new FailRes({ title: "The title already exists" }));
   }
-  /*const newAnime = new Anime({
-    title: req.body.title,
-    description: req.body.description,
+  var id = Math.floor(Math.random() * 90000) + 10000;
+  const newID = await Anime.findOne({
+    aniId: id,
   });
-  const result = await newAnime.save();
-  console.log(new SuccessRes(result));*/
+
+  if (newID !== null) {
+    return res.json(new FailRes({ aniId: "ID already in use" }));
+  }
+
   const result = await Anime.create({
+    aniId: id,
     title: req.body.title,
     description: req.body.description,
   });
