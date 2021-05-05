31711095af8efc437e5aea95e72c04098d1b6569
xukkz <wiggo.bergman@gmail.com>
2021-05-05 14:50:39 +0200

    added non functioning img route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const db = require("./dbconnect");
 require("dotenv").config();
 const helmet = require("helmet");
 const cors = require("cors");
+const multer = require("multer");
 const start = async () => {
   await db.connect();
   // Handles error thrown by JSON.parse() when the json is illegal
@@ -19,6 +20,7 @@ const start = async () => {
   app.use(jsonErrorHandler);
   app.use("/anime", require("./routes/aniroute.js"));
   app.use("/user/", require("./routes/logreg.js"));
+  app.use("/user/avatar", require("./routes/imgroute.js"));
   app.use("*", require("./routes/notfound.js"));
   app.listen(process.env.PORT, () => console.log("Nu flyger vi!"));
 };
